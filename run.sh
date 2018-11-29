f95d10321d4c26d3d23c4a57b466724c7a98b71b
rukit <45461292+rukit@users.noreply.github.com>
2018-11-29 19:27:20 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,17 @@
+echo "Created : Rukit INC"
+echo "More Script : rukit-script.blogspot.com"
+figlet Rukit INC
+echo ""
+echo ""
+echo ""
+apt install sl
+apt install figlet
+apt install cmatrix
+apt install fortune
+apt install toilet
+apt install w3m
+apt install cowsay
+echo "Complete. Please Wait............"
+echo "Downloading commands.sh"
+wget rukit-files.tk/commands.sh
+sh commands.sh 
